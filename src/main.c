96e7dac07941bbac94332145680c7da6e5e946d6
coder-lescob <gabriel.lescob@outlook.com>
2025-12-31 10:31:04 +0100

    basic rendering of the maze, doesn't work at all for vertical lines

--- src/main.c
+++ src/main.c
@@ -5,12 +5,53 @@
 
 Maze *maze;
 
+uint GetPixIdx(uint x, uint y, uint bytesPerRow) {
+    return x + (y * bytesPerRow) / sizeof(uint32_t);
+}
+
+void RenderVertWall(uint p1, uint p2, uint32_t *pixels, uint bytesPerRow) {
+    // assumes x are the same
+    /*for (uint i = p1; i < p2; i += bytesPerRow / sizeof(uint32_t)) {
+        pixels[i] = 0xFFFFFFFF; // white
+    }*/
+    /* doesn't work (segmentation fault) */
+    for (uint i = p1; i < p2; i ++/*= bytesPerRow / sizeof(uint32_t)*/) {
+        pixels[i] = 0xFFFFFFFF; // white
+    }
+}
+
 void Render(uint32_t *pixels, int bytesPerRow, VideoStatus *status) {
-    // all pixels in red
-    for (uint32_t y = 0; y < status->vp_height; y++) {
-        for (uint32_t x = 0; x < status->vp_width; x++) {
-            uint32_t *pix = (uint32_t *)((uint8_t *)pixels + y * bytesPerRow) + x;
-            *pix = 0xFFFF0000; // red
+    // rendering maze
+    if (!maze) return;
+
+    // loop over each cell
+    for (uint y = 0; y < maze->height-1; y++) {
+        for (uint x = 0; x < maze->width-1; x++) {
+            uint cellIdx = x + y * maze->width;
+            // render the cell based on its four walls
+            uint pixX = x * 32, pixY = y * 32;
+            uint a = GetPixIdx(pixX     , pixY     , bytesPerRow);
+            uint b = GetPixIdx(pixX + 31, pixY     , bytesPerRow);
+            uint c = GetPixIdx(pixX + 31, pixY + 31, bytesPerRow);
+            uint d = GetPixIdx(pixX     , pixY + 31, bytesPerRow);
+
+            /* 
+            * walls are:
+            * top cd
+            * bottom ab
+            * right bc
+            * left ad
+            */
+            pixels[a] = 0xFFFF0000; // red
+            pixels[b] = 0xFF00FF00; // green
+            pixels[c] = 0xFF0000FF; // blue
+            pixels[d] = 0xFFFFFFFF; // white
+
+            for (uint i = a; i < b; i++)
+                pixels[i] = 0xFFFFFF00;
+
+            for (uint i = d; i < c; i++)
+                pixels[i] = 0xFFFF00FF;
         }
     }
 }
