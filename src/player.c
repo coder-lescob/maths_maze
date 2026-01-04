63ab3448163503178c60592953e68b31344178d0
coder-lescob <gabriel.lescob@outlook.com>
2026-01-04 15:34:34 +0100

    commenting code

--- src/player.c
+++ src/player.c
@@ -21,25 +21,32 @@ void UpdatePlayer(Player *player, const Uint8 *keyboard, Maze *maze) {
 }
 
 void ResolveCollisons(Player *player, Maze *maze) {
+    // compute the position of the player in the cells grid
     uint cellX = (uint32_t)player->x / CellSize, cellY  = (uint32_t)player->y / CellSize;
-    uint cellIdx = cellX + cellY * maze->width;
 
+    // if the player is somehow outside of the maze don't check collison
     if (cellX >= maze->width || cellY >= maze->height) return;
 
+    // get the cell from its index
+    uint cellIdx = cellX + cellY * maze->width;
     Cell cell = maze->cells[cellIdx];
 
+    // if there is a left wall and that the player clipped into it snaps him back
     if (cell.leftWall && player->x < cellX * CellSize + HalfPlayerSize) {
         player->x = cellX * CellSize + HalfPlayerSize;
     }
 
+    // if there is a right wall and that the player clipped into it snaps him back
     if (cell.rightWall && player->x > (cellX + 1) * CellSize - HalfPlayerSize) {
         player->x = (cellX + 1) * CellSize - HalfPlayerSize;
     }
-
+    
+    // if there is a bottom wall and that the player clipped into it snaps him back
     if (cell.bottomWall && player->y < cellY * CellSize + HalfPlayerSize) {
         player->y = cellY * CellSize + HalfPlayerSize;
     }
 
+    // if there is a top wall and that the player clipped into it snaps him back
     if (cell.topWall && player->y > (cellY + 1) * CellSize - HalfPlayerSize) {
         player->y = (cellY + 1) * CellSize - HalfPlayerSize;
     }
